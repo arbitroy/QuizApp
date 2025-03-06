ef5d8c0dd9cfbc69be315d6c9eeb3510389bad5d
arbitroy <ndauwa.austine20@students.dkut.ac.ke>
2025-03-06 17:46:00 +0300

    end of day

--- obj/Debug/net9.0/QuizApp.AssemblyInfo.cs
+++ obj/Debug/net9.0/QuizApp.AssemblyInfo.cs
@@ -14,7 +14,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("QuizApp")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+f86191d46af97425305be22054ef07a683df29ef")]
 [assembly: System.Reflection.AssemblyProductAttribute("QuizApp")]
 [assembly: System.Reflection.AssemblyTitleAttribute("QuizApp")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
